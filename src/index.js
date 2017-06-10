703bf5fb13fd0f81235d7bade7fe11c7fa7ae030
윤상훈 <shygiants@gmail.com>
2017-06-11 02:32:25 +0900

    Support encoded images

--- src/index.js
+++ src/index.js
@@ -30,41 +30,77 @@ class Client {
 
   /**
    * 
-   * @param {Buffer} image
+   * @param {Buffer|Array<Buffer>} images
    * @param {string} name 
+   * @param {{decode: boolean, version: number}} [option]
    * @return {Promise}
    */
-  predict(image, name) {
-    const decoded = jpeg.decode(image, true);
+  predict(images, name, {decode, version}) {
+    decode = decode || false;
+    version = version || 1;
+    let imagesMsg;
+    if (decode) {
+      const decoded = jpeg.decode(images, true);
+      imagesMsg = {
+        dtype: 'DT_INT32',
+        tensor_shape: {
+          dim: [
+            { size: decoded.height },
+            { size: decoded.width },
+            { size: 3 },
+          ]
+        },
+        int_val: Array.from(decoded.data)
+      }
+    } else {
+      if (!(images instanceof Array)) {
+        images = [images];
+      }
+      imagesMsg = {
+        dtype: 'DT_STRING',
+        tensor_shape: {
+          dim: {
+            size: images.length
+          }
+        },
+        string_val: images
+      }
+    }
+
+    
     const predictRequest = {
       model_spec: {
         name,
-        signature_name: SIGNATURE_NAME
+        signature_name: SIGNATURE_NAME,
+        version: {
+          value: version
+        }
       },
       inputs: {
-        images: {
-          dtype: 'DT_INT32',
-          tensor_shape: {
-            dim: [
-              { size: decoded.height },
-              { size: decoded.width },
-              { size: 3 },
-            ]
-          },
-          int_val: Array.from(decoded.data)
-        }
+        images: imagesMsg
       }
     }
 
     return new Promise((resolve, reject) => {
       this.client.predict(predictRequest, (err, res) => {
         if (err) return reject(err);
-        resolve(res.outputs.scores.float_val);
+
+        const {scores} = res.outputs;
+        const {float_val, tensor_shape} = scores;
+
+        if (decode) {
+          resolve(float_val);
+        } else {
+          const numLabels = Number(tensor_shape.dim[1].size);
+          const vals = [...Array(images.length).keys()].map(idx => 
+            float_val.slice(numLabels * idx, numLabels * (idx + 1))
+          )
+          resolve(vals);
+        }
       });
     });
   }
 }
-
 
 if (require.main === module) {
   // Run directly from Node.js
@@ -78,10 +114,14 @@ if (require.main === module) {
   // Predict
   const client = TFServing.connect(config.get('connection'));
   client
-    .predict(image, config.get('modelName'))
+    .predict(image, 
+             config.get('modelName'), 
+             { version: config.get('version') })
     .then(vals => {
-      console.log(`Scores: ${vals}`);
-      console.log(`Argmax: ${argmax(vals)}`);
+      vals.forEach(val => {
+        console.log(`Scores: ${val}`);
+        console.log(`Argmax: ${argmax(val)}`);
+      });
     })
     .catch(console.error);
 }
